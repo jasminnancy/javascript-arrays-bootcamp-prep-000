b93e56710465c09e0f16e1f7c655f4b5d0efa775
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-19 14:37:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(chocolateBars, a) {
   chocolateBars.unshift(a)
+  return chocolateBars
 }
 function destructivelyAddElementToBeginingOfArray(chocolateBars, a) {
   return [a, ...chococlateBars]
