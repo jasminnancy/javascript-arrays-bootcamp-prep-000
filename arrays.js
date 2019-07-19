e3b3c7839b34ab44e45e0ca6a0d40e3f6a3cbeb6
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-19 15:09:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,6 +29,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 function removeElementFromEndOfArray(array) {
-  array.slice(1)
+  array.slice(0, -1)
   return array
 }
